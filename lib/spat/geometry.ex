317ea94f976b2a223a6579f690e9398ab66273e1
Stefan Johnson <sj@scrimpycat.io>
2018-07-02 17:18:15 +1000

    Add geometry behaviour

---
+++ lib/spat/geometry.ex
@@ -0,0 +1,7 @@
+defmodule Geometry do
+    @doc """
+      Implement the behaviour for calculating the indexes of the requested
+      geometry.
+    """
+    @callback index(geometry :: any, bounds :: Spat.Coord.t, subdivisions :: pos_integer) :: [Spat.subdivision(Spat.grid_index)]
+end
