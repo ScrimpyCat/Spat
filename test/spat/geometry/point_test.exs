c2711149be722e13b4c41a224d4bd238ff527f72
Stefan Johnson <sj@scrimpycat.io>
2018-07-08 01:42:26 +1000

    Test point intersection

--- test/spat/geometry/point_test.exs
+++ test/spat/geometry/point_test.exs
@@ -1,4 +1,18 @@
 defmodule Spat.Geometry.PointTest do
     use ExUnit.Case
     doctest Spat.Geometry.Point
+
+    test "point intersection" do
+        assert true == Spat.Geometry.Point.intersect({ 0 }, { 0 }, { 1 }, 1)
+        assert true == Spat.Geometry.Point.intersect({ 1 }, { 0 }, { 1 }, 1)
+        assert true == Spat.Geometry.Point.intersect({ 0.5 }, { 0 }, { 1 }, 1)
+        assert false == Spat.Geometry.Point.intersect({ -1 }, { 0 }, { 1 }, 1)
+        assert false == Spat.Geometry.Point.intersect({ 1.1 }, { 0 }, { 1 }, 1)
+
+        assert false == Spat.Geometry.Point.intersect({ 0, 0, 0 }, { 0, 1, 2 }, { 1, 2, 3 }, 3)
+        assert false == Spat.Geometry.Point.intersect({ 0, 1, 0 }, { 0, 1, 2 }, { 1, 2, 3 }, 3)
+        assert true == Spat.Geometry.Point.intersect({ 0, 1, 2 }, { 0, 1, 2 }, { 1, 2, 3 }, 3)
+        assert true == Spat.Geometry.Point.intersect({ 10, 10, 10 }, { 0, 1, 2 }, { 10, 10, 10 }, 3)
+        assert false == Spat.Geometry.Point.intersect({ 10, 10, 10.1 }, { 0, 1, 2 }, { 10, 10, 10 }, 3)
+    end
 end
